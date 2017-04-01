deac6b5fcaa3cd9cb87aadf8268d5e4a9a95dde6
Ishmukhametov <dinis.ish@gmail.com>
2017-04-02 02:44:31 +0500

    connector

--- rxgo.go
+++ rxgo.go
@@ -2,37 +2,73 @@ package rxgo
 
 import "sync"
 
-type Observable chan interface{}
-
-func (o Observable) fromTo(from, to int) Observable {
-	c := o
-	go func() {
-		for i := from; i <= to; i++ {
-			c <- i
-		}
-		close(c)
-	}()
-	return c
+type Connect struct {
 }
 
-func FromTo(from, to int) Observable {
-	o := make(Observable)
-	o.fromTo(from, to)
+type Observable chan interface{}
+type Obsrver chan Observable
+
+type Connector chan Observable
+type Emitter chan Observable
+
+func (o Observable) fromTo(from, to int) Observable {
+	go func() {
+		for i := from; i <= to; i++ {
+			o <- i
+		}
+		close(o)
+	}()
 	return o
 }
 
-func (o Observable) Map(f func(interface{}) interface{}) Observable {
-	out := make(Observable)
+func (in Connector) Start(out Connector, f func(Observable)) {
 	go func() {
 		for {
-			i, ok := <-o
-			if !ok {
-				close(out)
-				return
-			}
-			out <- f(i)
+			o := <-in
+			out <- o
+			go f(o)
 		}
 	}()
+}
+
+func (in Connector) Between(out Connector, f func(Observable) Observable) {
+	go func() {
+		for {
+			o := <-in
+			go func() { out <- f(o) }()
+		}
+	}()
+}
+
+func (in Connector) Range(from, to int) Connector {
+	out := make(Connector)
+	fromTo := func(o Observable) {
+		for i := from; i <= to; i++ {
+			o <- i
+		}
+		close(o)
+	}
+	in.Start(out, fromTo)
+	return out
+}
+
+func (in Connector) Map(f func(interface{}) interface{}) Connector {
+	out := make(Connector)
+	work := func(o Observable) Observable {
+		out1 := make(Observable)
+		go func() {
+			for {
+				i, ok := <-o
+				if !ok {
+					close(out1)
+					break
+				}
+				out1 <- f(i)
+			}
+		}()
+		return out1
+	}
+	in.Between(out, work)
 	return out
 }
 
@@ -53,21 +89,29 @@ func (o Observable) Filter(f func(interface{}) bool) Observable {
 	return out
 }
 
-func (o Observable) ToSlice() Observable {
-	out := make(Observable)
-	go func() {
-		t := make([]interface{}, 0)
-		for {
-			i, ok := <-o
-			if ok {
-				t = append(t, i)
-			} else {
-				out <- t
-				close(out)
-				return
+func (in Connector) ToSlice() Connector {
+	out := make(Connector)
+
+	f := func(o Observable) Observable {
+		out := make(Observable)
+		go func() {
+			t := make([]interface{}, 0)
+			for {
+				i, ok := <-o
+				if ok {
+					t = append(t, i)
+				} else {
+					out <- t
+					close(out)
+					return
+				}
 			}
-		}
-	}()
+		}()
+		return out
+	}
+
+	in.Between(out, f)
+
 	return out
 }
 
@@ -154,14 +198,17 @@ func Use(s *Source) Observable {
 	return out
 }
 
-func (o Observable) Subscribe(f func(interface{})) {
+func (out Connector) Subscribe(f func(interface{})) {
 	go func() {
 		for {
-			i, ok := <-o
-			if !ok {
-				return
+			o := <-out
+			for {
+				i, ok := <-o
+				if !ok {
+					return
+				}
+				f(i)
 			}
-			f(i)
 		}
 	}()
 }
